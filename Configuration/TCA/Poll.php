d47ea50a84968ff541f53a02b8c32e8d0ae1cef1
Jon Klixbüll Langeland <jon@moc.net>
2013-08-08 11:11:15 +0200

    [!!!][FEATURE] Compatibility with TYPO3 6.1
    Contains database changes
    
    Change-Id: I71d18ecf584b386ecd70d23bd4995dbcece412d4

---
+++ Configuration/TCA/Poll.php
@@ -0,0 +1,35 @@
+<?php
+$TCA['tx_mocpoll_domain_model_poll'] = array(
+	'ctrl'      => $TCA['tx_mocpoll_domain_model_poll']['ctrl'],
+	'interface' => array('showRecordFieldList' => 'question,responses'),
+	'columns' => array(
+		'question' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:moc_poll/Resources/Private/Language/Poll/locallang.xlf:question',
+			'config' => array(
+				'type' => 'input',
+				'size' => 40,
+				'eval' => 'trim'
+			)
+		),
+		'responses' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:moc_poll/Resources/Private/Language/Poll/locallang.xlf:responses',
+			'config' => array(
+				'type' => 'inline',
+				'foreign_field' => 'poll',
+				'foreign_table' => 'tx_mocpoll_domain_model_response',
+				'allowed' => 'tx_mocpoll_domain_model_response',
+				'maxitems' => '99999'
+			)
+		),
+		'parent' => array(
+			'exclude' => 0,
+			'config' => array(
+				'type' => 'passthrough'
+			)
+		)
+	),
+	'types' => array('1' => array('showitem' => 'question,responses')),
+	'palettes' => array('1' => array('showitem' => ''))
+);
